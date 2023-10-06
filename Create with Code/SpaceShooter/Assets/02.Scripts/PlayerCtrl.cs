c7515984f55e13f5479e82c9c393d8a598b78ca9
CHYIJE <118803252+CHYIJE@users.noreply.github.com>
2023-10-06 17:43:57 +0900

    p136
    p136

--- Create with Code/SpaceShooter/Assets/02.Scripts/PlayerCtrl.cs
+++ Create with Code/SpaceShooter/Assets/02.Scripts/PlayerCtrl.cs
@@ -6,11 +6,15 @@ public class PlayerCtrl : MonoBehaviour
 {
     private float h = 0.0f;
     private float v = 0.0f;
+    private float r = 0.0f;
 
     // 접근해야 하는 컴포넌트는 반드시 변수에 할당한 후 사용
     private Transform tr;
     // 이동 속도 변수(publick으로 선언되어 Inspector에 노출됨)
     public float moveSpeed = 10.0f;
+
+    // 회전 속도 변수
+    public float rotSpeed = 80.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +29,18 @@ public class PlayerCtrl : MonoBehaviour
     {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
+        r = Input.GetAxis("Mouse X");
 
         Debug.Log("h=" + h.ToString());
         Debug.Log("v=" + v.ToString());
 
+        // 전후좌우 이동 방향 벡터 계산
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
         // Translate(이동방향 * 속도 * 변위값 * Time.delataTime, 기준좌표)
-        tr.Translate(Vector3.forward * moveSpeed * v * Time.deltaTime, Space.Self);
+        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+
+        // Vector3.up 축을 기준으로 rotSpeed만큼의 속도로 회전
+        tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
     }
 }
